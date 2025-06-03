05d2c8161e1c1c84a52f057bc94b7d3ab3163348
Adam Lyon <adam@nullrider.com>
2025-06-02 20:50:51 -0600

    Feature updates
    Added ability to update the background color without clearing the container.  Right clicking erases colored cells

--- ALGORITHM.md
+++ ALGORITHM.md
@@ -11,3 +11,37 @@ I don't have enough experience to lay this all out at once so I'll keep this doc
 - Populate index.html with boilplate
 - Insert style and script tags
 
+# The Grid
+Per the instructions we are to create the grid entirely through script.  All other functionality hinges on this.
+
+Steps:
+Create div in html to contain the grid
+
+In the script
+put desired grid count into a variable (it may change)
+
+Run for loop to create rows
+    Run for loop to create each cell in each row
+        Insert individual divs into row
+    Insert row into container
+
+OK, this basically worked.  I now have a grid
+Some hoops I had to jump through
+- Creating each row and cell with 'flex: 1'
+
+Other than that it worked how I expected
+
+# The events
+We are talking about upto 10,000 cells.  We don't want an event handler on each one.  So my plan is to set the event handler on the grid container and user event.target to set the background color.
+
+Pseudo code
+Set eventListener on gridcontainer
+Call event.target.style.backgroudColor = "color" on it.
+
+Let's see how that works.
+
+Ok, that works, but also doesn't
+
+New plan, we use 'mouseOver' no click initially
+
+Totally worked!  Had to tweak a few other things, but the core functionality is in place
--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 
 const resizeGridButton = document.querySelector("#resizeGridButton")
+const updateBackgroundButton = document.querySelector("#updateBackgroundButton");
 
 const gridContainer = document.querySelector("#gridContainer");
 
 const containerSize = "500px";
 
 let gridCount = 100;
-let backgroundColor = "#00FFFF";
+let paintColor = "#00FFFF";
+let gridBackgroundColor = "rgb(255, 255, 255)";
 
 let mouseDown = false;
 let randomColor = true;
 
 gridContainer.style.cssText = `display: flex; flex-direction: column; \
-                                width: ${containerSize}; height: ${containerSize}`;
+                                width: ${containerSize}; height: ${containerSize}; \
+                                cursor: url('images/paintbrush.png'), auto;`;
 
 function resizeGrid() {
     // Set GridContainer Attributes
@@ -23,8 +26,10 @@ function resizeGrid() {
         newRow.style.cssText = "display: flex; flex: 1;";
         for (let cols = 0; cols < gridCount; cols++) {
             let newCell = document.createElement("div");
+            newCell.id = "cell";
             // For now we'll create the div with a border we can see
             newCell.style.cssText = "flex: 1; user-select: none;";
+            newCell.style.backgroundColor = gridBackgroundColor;
             //newCell.style.cssText = "border: black 1px solid";
             // Add cell to row
             newRow.appendChild(newCell);
@@ -38,18 +43,41 @@ resizeGridButton.addEventListener("click", resizeGrid);
 
 gridContainer.addEventListener("mousemove", function (event) {
     if (event.buttons === 1) {
-        let useBackgroundColor = backgroundColor;
+        let usePaintColor = paintColor;
         if (randomColor) {
             const red = Math.random() * 255;
             const green = Math.random() * 255;
             const blue = Math.random() * 255;
-            useBackgroundColor = `rgb(${red}, ${green}, ${blue})`;
+            usePaintColor = `rgb(${red}, ${green}, ${blue})`;
         }
-        event.target.style.backgroundColor = useBackgroundColor;
+        event.target.style.backgroundColor = usePaintColor;
+    } else if (event.buttons === 2) {
+        event.target.style.backgroundColor = gridBackgroundColor;
     }
 });
 
+gridContainer.addEventListener("contextmenu", function (event) {
+    event.preventDefault();
+});
+
+function updateBackground(newColor) {
+    console.log("Updating Background");
+    const cells = document.querySelectorAll("#cell");
+    const cellArr = Array.from(cells);
+    for (let cell of cellArr) {
+        if (cell.style.backgroundColor === gridBackgroundColor) {
+            cell.style.backgroundColor = newColor;
+        }
+    }
+}
+updateBackgroundButton.addEventListener("click", () => {
+
+    updateBackground("#00FF00")
+});
+
 resizeGrid();
+
+
 
 // gridContainer.addEventListener("mousedown", () => {
 //     mouseDown = true;
